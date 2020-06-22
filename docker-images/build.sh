2d281d2d58c5591ed82122d263f9e88a58b1851f
Raúl Kripalani <raul@protocol.ai>
2020-06-22 22:00:37 +0100

    fix build.sh.

--- docker-images/build.sh
+++ docker-images/build.sh
@@ -24,4 +24,4 @@ fi
 dir="$(dirname "$0")"
 
 docker build -t "iptestground/oni-buildbase:$TAG" -f "$dir/Dockerfile.oni-buildbase" "$dir"
-docker build -t "iptestground/oni-buildbase:$TAG" -f "$dir/Dockerfile.oni-runtime" "$dir"
+docker build -t "iptestground/oni-runtime:$TAG" -f "$dir/Dockerfile.oni-runtime" "$dir"
