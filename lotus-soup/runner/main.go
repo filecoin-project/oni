a8dacbe815622d9372116aabfb58195a6ba717de
Yusef Napora <yusef@napora.org>
2020-07-13 15:36:06 -0400

    build composition before running

--- lotus-soup/runner/main.go
+++ lotus-soup/runner/main.go
@@ -55,25 +55,27 @@ func worker(id int, jobs <-chan jobDefinition, results chan<- jobResult) {
 	}
 }
 
+func buildComposition(compositionPath string, outputDir string) (string, error) {
+	outComp := path.Join(outputDir, "composition.toml")
+	err := sh.Command("cp", compositionPath, outComp).Run()
+	if err != nil {
+		return "", err
+	}
+
+	return outComp, sh.Command("testground", "build", "composition", "-w", "-f", outComp).Run()
+}
+
 func main() {
 	runs := flag.Int("runs", 1, "number of times to run composition")
 	parallelism := flag.Int("parallel", 1, "number of test runs to execute in parallel")
-	outputDir := flag.String("output", "", "path to output directory (will use temp dir if unset)")
+	outputDirFlag := flag.String("output", "", "path to output directory (will use temp dir if unset)")
 	flag.Parse()
 
 	if len(flag.Args()) != 1 {
 		log.Fatal("must provide a single composition file path argument")
 	}
 
-	compFile := flag.Args()[0]
-
-	jobs := make(chan jobDefinition, *runs)
-	results := make(chan jobResult, *runs)
-	for w := 1; w <= *parallelism; w++ {
-		go worker(w, jobs, results)
-	}
-
-	outdir := *outputDir
+	outdir := *outputDirFlag
 	if outdir == "" {
 		var err error
 		outdir, err = ioutil.TempDir(os.TempDir(), "oni-batch-run-")
@@ -81,11 +83,31 @@ func main() {
 			log.Fatal(err)
 		}
 	}
+	if err := os.MkdirAll(outdir, os.ModePerm); err != nil {
+		log.Fatal(err)
+	}
+
+	compositionPath := flag.Args()[0]
+
+	// first build the composition and write out the artifacts.
+	// we copy to a temp file first to avoid modifying the original
+	log.Printf("building composition %s\n", compositionPath)
+	compositionPath, err := buildComposition(compositionPath, outdir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	jobs := make(chan jobDefinition, *runs)
+	results := make(chan jobResult, *runs)
+	for w := 1; w <= *parallelism; w++ {
+		go worker(w, jobs, results)
+	}
+
 
 	for j := 1; j <= *runs; j++ {
 		dir := path.Join(outdir, fmt.Sprintf("run-%d", j))
 		skipStdout := *parallelism != 1
-		jobs <- jobDefinition{runNumber: j, compositionPath: compFile, outputDir: dir, skipStdout: skipStdout}
+		jobs <- jobDefinition{runNumber: j, compositionPath: compositionPath, outputDir: dir, skipStdout: skipStdout}
 	}
 	close(jobs)
 
